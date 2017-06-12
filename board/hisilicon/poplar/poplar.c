7bd1d9fe9f3ed58c68bd3005d4eaad5c2b68a005
Alex Elder <elder@linaro.org>
2017-06-12 16:03:38 -0500

    poplar: BL31 is now found lower in memory
    The FIP has been moved to a higher offset in the l-loader image, and
    as a consequence the BL images have been moved to locations lower in
    memory than before.  Update the definition of ATF_BL1_BASE to reflect
    the new location.
    
    Signed-off-by: Alex Elder <elder@linaro.org>

--- board/hisilicon/poplar/poplar.c
+++ board/hisilicon/poplar/poplar.c
@@ -55,7 +55,7 @@ int dram_init(void)
 }
 
 /* Base address and size of ARM-TF image that persists in memory */
-#define ATF_BL31_BASE	0x00129000
+#define ATF_BL31_BASE	0x00069000
 #define ATF_BL31_SIZE	0x00018000
 
 int dram_init_banksize(void)
